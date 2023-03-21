0e9318554d6e1386f9a7f38006364668701213c5
agustinmfran <agustinmfran@gmail.com>
2023-03-21 18:26:50 +0100

    prueba 2

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
+});
 app.use("/api/v1/movies", movies);
 app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
 
